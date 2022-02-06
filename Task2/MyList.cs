652939b8655ea6a4adcbe577f9859dfb268df559
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-06 19:23:30 +0300

    Add project files.

---
+++ Task2/MyList.cs
@@ -0,0 +1,26 @@
+﻿namespace Task2
+{
+    internal class MyList<T>
+    {
+        private T[] array;
+        private int lastIndex = 0;
+        public int Lenght { get => lastIndex; }
+        public void Add(T item)
+        {
+            array[lastIndex] = item;
+            lastIndex++;
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                return array[i];
+            }
+        }
+        public MyList()
+        {
+            array = new T[10];
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(40);
+
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[1]);
+            Console.WriteLine(list[2]);
+            Console.WriteLine(list[3]);
+            Console.WriteLine(list.Lenght);
+        }
+    }
+}
