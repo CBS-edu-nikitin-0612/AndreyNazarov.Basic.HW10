afbffda406c40901bf1d99c872896463c6d0b5bb
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-06 19:44:27 +0300

    feat: Task3

---
+++ Task3/MyDictionary.cs
@@ -0,0 +1,25 @@
+﻿namespace Task3
+{
+    internal class MyDictionary<K, V>
+    {
+        private KeyValuePair<K, V>[] array;
+        private int lastIndex = 0;
+        public int Length { get => lastIndex; }
+
+        public void Add(K key, V value)
+        {
+            array[lastIndex] = new KeyValuePair<K, V>(key, value);
+            lastIndex++;
+        }
+
+        public KeyValuePair<K, V> this[int i]
+        {
+            get => array[i];
+        }
+
+        public MyDictionary()
+        {
+            array = new KeyValuePair<K, V>[10];
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> dict = new MyDictionary<string, string>();
+            dict.Add("первый", "first");
+            dict.Add("второй", "second");
+            dict.Add("третий", "third");
+
+            Console.WriteLine(dict.Length);
+            Console.WriteLine(dict[1].Key);
+            Console.WriteLine(dict[2].Value);
+        }
+    }
+    internal class KeyValuePair<K, V>
+    {
+        public K Key { get; }
+        public V Value { get; }
+
+        public KeyValuePair(K key, V value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
