0f229e9ee3da13c19c54ddb0d81223408d89cb72
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-06 22:02:21 +0300

    feat: Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.Security;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyClass<int>.FacrotyMethod());
+        }
+    }
+
+    internal static class MyClass<T> where T : new()
+    {
+        public static T FacrotyMethod()
+        {
+            return new T();
+        }
+    }
+}
